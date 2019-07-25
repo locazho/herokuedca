e96dad4c9f9ca0a4b80b092fb8170ad4f40972fb
locazho <bryant_bmp@hotmail.com>
2019-07-25 14:00:57 -0600

    heroku prueba settings

--- portaledcahn/settings.py
+++ portaledcahn/settings.py
@@ -132,12 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = '/portaledcahn_frontend/static'
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'portaledcahn_frontend/static')
 
 DISABLE_COLLECTSTATIC=1
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
