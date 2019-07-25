aaea7595a92533edb18e185d81704af626a49770
locazho <bryant_bmp@hotmail.com>
2019-07-25 09:35:39 -0600

    heroku prueba settings

--- portaledcahn/settings.py
+++ portaledcahn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uo251c%zw60+efuwf$7yn7dl=6@0)p12%q(-87*p4r^dy-zbhp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.237.10.60']
+ALLOWED_HOSTS = ['35.237.10.60','127.0.0.1']
 
 
 # Application definition
@@ -136,6 +136,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'portaledcahn_frontend/static'),
 )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'portaledcahn_frontend/static')
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
