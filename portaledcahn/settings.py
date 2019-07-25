1ad70cbd0d72483236713938204103a4ce34bdde
locazho <bryant_bmp@hotmail.com>
2019-07-25 11:41:11 -0600

    heroku prueba settings

--- portaledcahn/settings.py
+++ portaledcahn/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
+"""STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'portaledcahn_frontend/static'),
-)
+)"""
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'portaledcahn_frontend/static')
 REST_FRAMEWORK = {
