78058d1b13e86858c6ba962f732d319f2ff04505
locazho <bryant_bmp@hotmail.com>
2019-07-26 09:42:33 -0600

    heroku prueba settings

--- edcahn_backend/settings.py
+++ edcahn_backend/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'portaledcahn_frontend')
 STATICFILES_DIRS=(
-    os.path.join(STATIC_ROOT,'/static'),
+    os.path.join(BASE_DIR,'portaledcahn_frontend'),
 )"""
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'portaledcahn_frontend/static'),
